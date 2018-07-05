1feb3725eda04e4de0970d4f7b8fa8386ac994a1
RedSponge <eran.godsi2003@gmail.com>
2018-07-05 23:46:26 +0300

    Better Pinging

--- src/main/java/com/redsponge/bot/HerokuServerHandler.java
+++ src/main/java/com/redsponge/bot/HerokuServerHandler.java
@@ -1,8 +1,15 @@
 package com.redsponge.bot;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 import javax.print.DocFlavor.INPUT_STREAM;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.URL;
 import java.util.Timer;
@@ -21,7 +28,10 @@ public class HerokuServerHandler {
     private static void createConnection() {
         try {
             System.out.println("CREATING SERVER!");
-            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(System.getenv("PORT")));
+            HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(System.getenv("PORT"))), 0);
+            server.createContext("/ping", new MyHandler());
+            server.setExecutor(null);
+            server.start();
             System.out.println("CREATED SERVER!");
         } catch (Exception e) {
             e.printStackTrace();
@@ -40,13 +50,27 @@ public class HerokuServerHandler {
                     InputStream in = null;
                     try {
                         System.out.println("Pinging");
+                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
                         in = url.openStream();
+                        System.out.println("Pinged");
+                        con.disconnect();
                         in.close();
                     } catch (IOException ignored) {}
                 }
             }, 0, 1000*60*5); //EVERY FIVE MINUTES
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Pong";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream out = exchange.getResponseBody();
+            out.write(response.getBytes());
+            out.close();
         }
     }
 
